3e2d3d3983419a85d2fed14e49c2ed1fb2f2c8b8
Iain Dorsch <i_dorsch21@informatik.uni-kl.de>
2024-01-09 19:27:32 +0100

    one solved minus complete 0 handling

--- one/one.c
+++ one/one.c
@@ -1,8 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int str_len(char * str)
+{
+    int i = 0;
+    while (str[i++]);
+    return i;
+}
+
+int search_str_int(char * str)
+{
+    int x;
+    for (int i = 0; i<str_len(str)-1;i++)
+    {
+        switch (atoi(&str[i]))
+        {
+        case 1:
+            x = 1;
+            return x;
+        case 2:
+            x = 2;
+            return x;
+        case 3:
+            x = 3;
+            return x;
+        case 4:
+            x = 4;
+            return x;
+        case 5:
+            x = 5;
+            return x;
+        case 6:
+            x = 6;
+            return x;
+        case 7:
+            x = 7;
+            return x;
+        case 8:
+            x = 8;
+            return x;
+        case 9:
+            x = 9;
+            return x;
+        case 0:
+            if (strcmp(&str[i], "0")!=0) {
+                break;
+            }
+            x = 0;
+            return x;
+        default:
+            break;
+        }
+    }    
+    return -1;
+}
+int search_str(char * str)
+{
+    char * strcp = (char *) malloc(str_len(str)*sizeof(char));
+    strcpy_s(strcp,str_len(str)*sizeof(char),str);
+    int x, y = 0;
+    x = search_str_int(str);
+    _strrev(strcp);
+    y = search_str_int(strcp);
+    return x*10+y;
+}
 
 int main()
 {
-    char * str1;
-    str1 = "234";
-    printf("%s",str1);
+    char * str1 = "1abc2";
+    char * str2 = "pqr3stu8vwx";
+    char * str3 = "a1b2c3d4e5f";
+    char * str4 = "treb7uchet";
+    char * str5 = "2akhsd0alsj";
+    
+    printf("x:%d\n",search_str(str3));
 }
